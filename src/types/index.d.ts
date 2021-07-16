562afb77215f8422e9b86a28fb882e7219bd9a7e
syoung125 <laura120555@gmail.com>
2021-07-16 10:02:00 +0900

    chore: :truck: rename type index.d.ts -> index.ts
    add celltype

--- src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export type FileExtensionType = 'xlsx' | 'csv';
-
-export type ExcelDownloadButtonProps = {
-  fileName: string;
-  fileExtension?: FileExtensionType;
-  data: any[][];
-  style?: React.CSSProperties;
-  element?: React.ReactElement;
-};
---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export type FileExtensionType = 'xlsx' | 'csv';
+export type CellType = string | number | boolean | Date;
+
+export type ExcelDownloadButtonProps = {
+  fileName: string;
+  fileExtension?: FileExtensionType;
+  data: CellType[][];
+  style?: React.CSSProperties;
+  element?: React.ReactElement;
+};
+
+export type ColumnsType<TData = Record<string, unknown>> = {
+  label: string;
+  key: string;
+  mapValue?: (record: TData) => CellType;
+}[];
